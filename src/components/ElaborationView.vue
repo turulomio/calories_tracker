ff8f6873a5366616bc600d8a8cda2c872483e44e
turulomio <turulomio@yahoo.es>
2022-12-06 10:18:41 +0100

    Split in TableElaborationsIngredients

--- src/components/ElaborationView.vue
+++ src/components/ElaborationView.vue
@@ -17,23 +17,8 @@
                     <v-tabs-items v-model="tab">
                         <v-tab-item key="ingredients">      
                             <v-card outlined>
-                                <v-data-table dense :headers="products_in_headers" :items="new_elaboration.elaborations_products_in" sort-by="final_grams" :sort-desc="['final_grams']" class="elevation-1" hide-default-footer disable-pagination :key="'T'+key" height="vh" fixed-header>
-                                    <template v-slot:[`item.products`]="{ item }"><div v-html="products_html_fullname(item.products,4)"></div></template>
-                                    <template v-slot:[`item.amount`]="{ item }">{{ fraction(item.amount).toFraction(true)}}</template>
-                                    <template v-slot:[`item.measures_types`]="{ item }"><div v-html="$store.getters.getObjectPropertyByUrl('measures_types', item.measures_types,'localname')"></div></template> 
-                                    <template v-slot:[`item.actions`]="{ item }">
-                                        <v-icon v-if="['C','U'].includes(product_in_mode)" small class="mr-2" @click="editProductIn(item)">mdi-pencil</v-icon>
-                                        <v-icon v-if="['C','U'].includes(product_in_mode)" small @click="deleteProductIn(item)">mdi-delete</v-icon>
-                                    </template>
-                                    <template v-slot:[`body.append`]="{headers}" v-if="new_elaboration.elaborations_products_in.length>0">
-                                        <tr style="background-color: WhiteSmoke">
-                                            <td v-for="(header,i) in headers" :key="i">
-                                                <div v-if="header.value=='products'">{{ $t("Total {0} products):").format(new_elaboration.elaborations_products_in.length) }}</div>
-                                                <div v-if="header.value == 'final_grams'" class="d-flex justify-end" v-html="listobjects_sum(new_elaboration.elaborations_products_in,'final_grams')"></div>
-                                            </td>
-                                        </tr>
-                                    </template>
-                                </v-data-table>
+                                <TableElaborationsIngredients ref="table_elaborations_ingredients" :elaboration="new_elaboration" :key="key" @cruded="on_TableElaborationsIngredients_cruded()"></TableElaborationsIngredients>
+
                             </v-card>
                         </v-tab-item>
                         <v-tab-item key="containers">      
@@ -57,26 +42,18 @@
             <v-card-actions>                
                 <v-btn color="error" :disabled="!new_elaboration.url" @click="createElaboratedProduct()" >{{ $t("Create an elaborated product") }}</v-btn>
                 <v-spacer></v-spacer>
-                <v-btn color="primary" :disabled="!new_elaboration.url" @click="addProductIn()" >{{ $t("Add an ingredient") }}</v-btn>
+                <v-btn color="primary" :disabled="!new_elaboration.url" @click="addIngredient()" >{{ $t("Add an ingredient") }}</v-btn>
                 <v-btn color="primary" :disabled="!new_elaboration.url" @click="addContainer()" >{{ $t("Add a container") }}</v-btn>
                 <v-btn color="primary" :disabled="!new_elaboration.url" @click="addElaborationStep()" >{{ $t("Add a step") }}</v-btn>
                 <v-btn color="primary" :disabled="!new_elaboration.url" @click="addExperience()" >{{ $t("Add a experience") }}</v-btn>
             </v-card-actions>
         </v-card>
-
-        <!-- DIALOG PRODUCTS_IN CRUD -->
-        <v-dialog v-model="dialog_products_in_crud" width="45%">
-            <v-card class="pa-4">
-                <ElaborationProductsInCRUD :product_in="product_in" :mode="product_in_mode" :key="'B'+key" @cruded="on_ElaborationProductsInCRUD_cruded"></ElaborationProductsInCRUD>
-            </v-card>
-        </v-dialog>
-
     </div>
 </template>
 <script>
     import axios from 'axios'
     import fraction from 'fraction.js'
-    import ElaborationProductsInCRUD from './ElaborationProductsInCRUD.vue'
+    import TableElaborationsIngredients from './TableElaborationsIngredients.vue'
     import MyMenuInline from './reusing/MyMenuInline.vue'
     import TableElaborationsContainers from './TableElaborationsContainers.vue'
     import TableElaborationsSteps from './TableElaborationsSteps.vue'
@@ -84,7 +61,7 @@
     import TableElaborationsExperiences from './TableElaborationsExperiences.vue'
     export default {
         components: {
-            ElaborationProductsInCRUD,
+            TableElaborationsIngredients,
             MyMenuInline,
             TableElaborationsContainers,
             TableElaborationsSteps,
@@ -123,27 +100,27 @@
                 new_elaboration: null,
                 tab: 2,
 
-                loading_products: false,
-
-
-                products_in_headers: [
-                    { text: this.$t('Product'), sortable: true, value: 'products'},
-                    { text: this.$t('Measure type'), value: 'measures_types', width:"20%"},
-                    { text: this.$t('Amount'), value: 'amount', align:'right', width:"10%"},
-                    { text: this.$t('Final grams'), value: 'final_grams', align:'right', width:"10%"},
-                    { text: this.$t('Actions'), value: 'actions', sortable: false, width:"8%"},
-                ],
-
-
                 key:0,
-                product_in:null,
-                product_in_mode:null,
-                dialog_products_in_crud:false,
             }
         },
         methods: {
             fraction,
             empty_elaborations_products_in,
+            async addIngredient(){
+                if (this.tab!=0){
+                    this.tab=0
+                    await new Promise(resolve => setTimeout(resolve, 1000));//Waits a second to mount table_links after tab change
+                }
+                this.$refs.table_elaborations_ingredients.addProductIn()
+
+            },
+            async addContainer(){
+                if (this.tab!=1){
+                    this.tab=1
+                    await new Promise(resolve => setTimeout(resolve, 1000));//Waits a second to mount table_links after tab change
+                }
+                this.$refs.table_elaborations_containers.on_new_click()
+            },
             async addElaborationStep(){
                 if (this.tab!=2){
                     this.tab=2
@@ -160,32 +137,7 @@
                 this.$refs.table_elaborations_experiences.addItem()
 
             },
-            addProductIn(){
-                this.tab=0
-                this.product_in=this.empty_elaborations_products_in()
-                this.product_in.measures_types=this.hyperlinked_url("measures_types",1)
-                this.product_in.elaborations=this.new_elaboration.url
-                this.product_in_mode='C'
-                this.key=this.key+1
-                this.dialog_products_in_crud=true
-
-            },
-            editProductIn(item){
-                this.product_in=item
-                this.product_in_mode='U'
-                this.key=this.key+1
-                this.dialog_products_in_crud=true
-
-            },
-            deleteProductIn(item){
-                this.product_in=item
-                this.product_in_mode='D'
-                this.key=this.key+1
-                this.dialog_products_in_crud=true
-
-            },
-            on_ElaborationProductsInCRUD_cruded(){
-                this.dialog_products_in_crud=false  
+            on_TableElaborationsIngredients_cruded(){
                 return axios.get(`${this.$store.state.apiroot}/api/elaborationsproductsinthrough/?elaboration=${this.new_elaboration.url}`, this.myheaders())
                 .then((response) => {
                     console.log(response.data)
@@ -216,8 +168,6 @@
                 });
             },
             on_TableElaborationsSteps_cruded(new_elaborations_steps){
-                console.log("on_Table_cruded")
-                console.log(new_elaborations_steps)
                 this.new_elaboration.elaborations_steps=new_elaborations_steps
                 axios.put(this.new_elaboration.url, this.new_elaboration,  this.myheaders())
                 .then((response) => {
@@ -240,12 +190,6 @@
                }, (error) => {
                     this.parseResponseError(error)
                 });
-            },
-            async addContainer(){                                    
-                console.log(this.$refs)
-                this.tab=1
-                await new Promise(resolve => setTimeout(resolve, 1000));//Waits a second to mount table_links after tab change
-                this.$refs.table_elaborations_containers.on_new_click()
             },
         },
         created(){
---
+++ src/components/TableElaborationsIngredients.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>    
+        <v-data-table dense :headers="products_in_headers" :items="elaboration.elaborations_products_in" sort-by="final_grams" :sort-desc="['final_grams']" class="elevation-1" hide-default-footer disable-pagination :key="'T'+key" height="vh" fixed-header>
+            <template v-slot:[`item.products`]="{ item }"><div v-html="products_html_fullname(item.products,4)"></div></template>
+            <template v-slot:[`item.amount`]="{ item }">{{ fraction(item.amount).toFraction(true)}}</template>
+            <template v-slot:[`item.measures_types`]="{ item }"><div v-html="$store.getters.getObjectPropertyByUrl('measures_types', item.measures_types,'localname')"></div></template> 
+            <template v-slot:[`item.actions`]="{ item }">
+                <v-icon small class="mr-2" @click="editProductIn(item)">mdi-pencil</v-icon>
+                <v-icon small @click="deleteProductIn(item)">mdi-delete</v-icon>
+            </template>
+            <template v-slot:[`body.append`]="{headers}" v-if="elaboration.elaborations_products_in.length>0">
+                <tr style="background-color: WhiteSmoke">
+                    <td v-for="(header,i) in headers" :key="i">
+                        <div v-if="header.value=='products'">{{ $t("Total {0} products):").format(elaboration.elaborations_products_in.length) }}</div>
+                        <div v-if="header.value == 'final_grams'" class="d-flex justify-end" v-html="listobjects_sum(elaboration.elaborations_products_in,'final_grams')"></div>
+                    </td>
+                </tr>
+            </template>
+        </v-data-table>
+
+        <!-- DIALOG PRODUCTS_IN CRUD -->
+        <v-dialog v-model="dialog_products_in_crud" width="45%">
+            <v-card class="pa-4">
+                <ElaborationProductsInCRUD :product_in="product_in" :mode="product_in_mode" :key="'B'+key" @cruded="on_ElaborationProductsInCRUD_cruded"></ElaborationProductsInCRUD>
+            </v-card>
+        </v-dialog>
+
+    </div>
+</template>
+<script>
+    import fraction from 'fraction.js'
+    import ElaborationProductsInCRUD from './ElaborationProductsInCRUD.vue'
+    import {empty_elaborations_products_in} from '../empty_objects.js'
+    export default {
+        components: {
+            ElaborationProductsInCRUD,
+        },
+        props: {
+            
+            elaboration: { 
+                required: true
+            },
+        },
+        data(){ 
+            return{
+                products_in_headers: [
+                    { text: this.$t('Product'), sortable: true, value: 'products'},
+                    { text: this.$t('Measure type'), value: 'measures_types', width:"20%"},
+                    { text: this.$t('Amount'), value: 'amount', align:'right', width:"10%"},
+                    { text: this.$t('Final grams'), value: 'final_grams', align:'right', width:"10%"},
+                    { text: this.$t('Actions'), value: 'actions', sortable: false, width:"8%"},
+                ],
+
+
+                key:0,
+                product_in:null,
+                product_in_mode:null,
+                dialog_products_in_crud:false,
+            }
+        },
+        methods: {
+            fraction,
+            empty_elaborations_products_in,
+            addProductIn(){
+                this.tab=0
+                this.product_in=this.empty_elaborations_products_in()
+                this.product_in.measures_types=this.hyperlinked_url("measures_types",1)
+                this.product_in.elaborations=this.elaboration.url
+                this.product_in_mode='C'
+                this.key=this.key+1
+                this.dialog_products_in_crud=true
+
+            },
+            editProductIn(item){
+                this.product_in=item
+                this.product_in_mode='U'
+                this.key=this.key+1
+                this.dialog_products_in_crud=true
+
+            },
+            deleteProductIn(item){
+                this.product_in=item
+                this.product_in_mode='D'
+                this.key=this.key+1
+                this.dialog_products_in_crud=true
+
+            },
+            on_ElaborationProductsInCRUD_cruded(){
+                this.dialog_products_in_crud=false  
+                this.key=this.key+1
+                this.$emit("cruded")
+            },
+        },
+        created(){
+        }
+    }
+</script>
+
