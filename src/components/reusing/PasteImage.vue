4039bccd2dea234db04340f7d58211c84ae1c80e
turulomio <turulomio@yahoo.es>
2022-12-08 09:46:03 +0100

    Adding main image to recipe

---
+++ src/components/reusing/PasteImage.vue
@@ -0,0 +1,105 @@
+<!--
+    Returns a js object with base64 image, mame and jsimage
+
+    Can set RulesString to component to set required
+-->
+
+<template>
+
+    <div>
+        <v-container class="d-flex">
+            <v-row class="flex-nowrap"  align-self="center">
+                <v-col align-self="center"><v-text-field :readonly="text_readonly"  @paste="pasteFunction" v-model="text" outlined prepend-icon="mdi-image" append-outer-icon="mdi-backspace"  @click:append-outer="on_text_backspace"  autofocus :placeholder="$t('Press Ctrl-V to paste an image')" :label="$t('Press Ctrl-V to paste an image')" :rules="$attrs.rules"></v-text-field></v-col>
+                <v-col class="flex-grow-0" >
+                    <v-card outlined >
+                        <v-img  contain  :width="height" :height="height" :src="new_image.jsimage"></v-img>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                new_image:this.empty_image(),
+                text:"",
+                text_readonly:false,
+            }
+        },
+        props: {
+            height: { // Persons merge
+                required: false,
+                default: 200,
+            },
+        },
+        methods:{
+            empty_image(){
+                return {
+                    image: null,
+                    mime:null,
+                    jsimage:null, //String with mime
+                }
+            },
+            on_text_backspace(){
+                this.text=""
+                this.text_readonly=false
+                this.new_image=this.empty_image()
+                console.log("AHORA")
+            },
+            async pasteFunction(pasteEvent, callback){
+
+                if(pasteEvent.clipboardData == false){
+                    if(typeof(callback) == "function"){
+                        console.log('Undefined ')
+                        callback(undefined);
+                    }
+                }
+
+                var items = pasteEvent.clipboardData.items
+
+                if(items == undefined){
+                    if(typeof(callback) == "function"){
+                        callback(undefined)
+                        console.log('Undefined 2')
+                    }
+                }
+                for (var i = 0; i < items.length; i++) {
+                    
+                    if (items[i].type.indexOf("image") == -1) continue
+                    var blob = items[i].getAsFile()
+                    this.new_image= await this.addImage(blob)
+                    console.log(this.new_image)
+                    this.text=this.$t("[Image pasted]")
+                    this.text_readonly=true
+                    this.$emit('input',this.new_image)
+                }
+            },
+            addImage(file){
+                return new Promise((resolve, reject) => {
+                    var reader = new FileReader();
+
+                    reader.onload = function() {
+                        const result=reader.result
+                        var r={
+                            jsimage: result,
+                            image: result.split(",")[1],
+                            mime: result.split(";base64,")[0].split(":")[1],
+                        }
+                        return resolve(r)
+
+                    }
+                    reader.onerror=function(error){
+                        return reject(error)
+                    }
+
+                    reader.readAsDataURL(file)
+                })
+
+            },
+        },
+    }
+</script>
