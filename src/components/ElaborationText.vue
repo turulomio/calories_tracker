b7def42a403ef868c38a44c9d726ecb75c7f6faf
turulomio <turulomio@yahoo.es>
2023-07-10 13:59:31 +0200

    Elaboration text improved

--- src/components/ElaborationText.vue
+++ src/components/ElaborationText.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        <mavon-editor v-model="text" language="en" :key="key" >
-            <template slot="right-toolbar-after">
+        <mavon-editor ref="me" v-model="text" language="en" :key="key" :toolbars="toolbars" >
+            <template slot="left-toolbar-after">
                 <button
                     type="button"
                     @click="on_btn_save_click"
-                    class="op-icon fa fa-mavon-align-left"
+                    class="op-icon fa fa-mavon-floppy-o"
                     aria-hidden="true"
                     :title="$t('Save')"
+                ></button>
+                <button
+                    type="button"
+                    @click="on_btn_add_text('HOLA')"
+                    class="mdi-eye"
+                    aria-hidden="true"
+                    :title="$t('Add text')"
                 ></button>
             </template>
         </mavon-editor>
@@ -29,10 +36,47 @@
             return{
                 text:"",
                 key:0,
+
+                toolbars: {
+                    bold: true,
+                    italic: true,
+                    header: true,
+                    underline: true,
+                    strikethrough: true,
+                    mark: true,
+                    superscript: true,
+                    subscript: true,
+                    quote: true,
+                    ol: true,
+                    ul: true,
+                    link: true,
+                    imagelink: true,
+                    code: true,
+                    table: true,
+                    fullscreen: true,
+                    readmodel: true,
+                    htmlcode: true,
+                    help: true,
+                    /* 1.3.5 */
+                    undo: true,
+                    redo: true,
+                    trash: true,
+                    save: false,
+                    /* 1.4.2 */
+                    navigation: true,
+                    /* 2.1.8 */
+                    alignleft: true,
+                    aligncenter: true,
+                    alignright: true,
+                    /* 2.2.1 */
+                    subfield: true,
+                    preview: false
+                }
             }
         },
         methods: {
-            on_btn_save_click(){ 
+            on_btn_save_click(a,b,c){ 
+                console.log(a,b,c)
                 var elaboration_text={
                     elaboration: this.elaboration.url,
                     text: this.text
@@ -48,12 +92,21 @@
                 } else {
                     axios.put(this.elaboration.elaborations_texts.url, elaboration_text,  this.myheaders())
                     .then(() => {
-                        // this.$emit("cruded")
                     }, (error) => {
                         this.parseResponseError(error)
                     })
                 }
             },
+            on_btn_add_text(text){
+                console.dir(this.$refs.me)
+                let insert_text = {
+                    prefix: "",
+                    subfix: '',
+                    str: text,
+                };
+                this.$refs.me.insertText(this.$refs.me.getTextareaDom(), insert_text)
+                this.$refs.me.getTextareaDom().selectionStart=this.$refs.me.getTextareaDom().selectionEnd
+            }
         },
         created(){
             if (this.elaboration.elaborations_texts){
