681ddf3b0841e922a469327210bd9b532bae168d
turulomio <turulomio@yahoo.es>
2023-07-10 14:28:25 +0200

    Elaboration text improved

--- src/components/ElaborationText.vue
+++ src/components/ElaborationText.vue
@@ -1,23 +1,56 @@
 <template>
-    <div>
-        <mavon-editor ref="me" v-model="text" language="en" :key="key" :toolbars="toolbars" >
-            <template slot="left-toolbar-after">
-                <button
-                    type="button"
-                    @click="on_btn_save_click"
-                    class="op-icon fa fa-mavon-floppy-o"
-                    aria-hidden="true"
-                    :title="$t('Save')"
-                ></button>
-                <button
-                    type="button"
-                    @click="on_btn_add_text('HOLA')"
-                    class="mdi-eye"
-                    aria-hidden="true"
-                    :title="$t('Add text')"
-                ></button>
-            </template>
-        </mavon-editor>
+    <div class="d-flex" >
+
+        <v-card width="20%" 
+       style="max-height: 300px"
+       class="overflow-y-auto">
+            <v-list >
+                <v-subheader>{{ $t("Ingredients") }}</v-subheader>
+
+                <v-list-item-group  color="primary" >
+                    <v-list-item v-for="(item, i) in elaboration.elaborations_products_in" :key="i" @click="on_ingredient_click(item)" >
+                        <v-list-item-icon>
+                            <v-icon >mdi-eye</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title >{{ item.fullname }}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+                <v-subheader>{{ $t("Containers") }}</v-subheader>
+
+                <v-list-item-group  color="primary" >
+                    <v-list-item v-for="(item, i) in elaboration.elaborations_containers" :key="i" @click="on_container_click(item)" >
+                        <v-list-item-icon>
+                            <v-icon >mdi-eye</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title >{{ item.name }}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-card>
+        <v-card width="80%" >
+            <mavon-editor ref="me" v-model="text" language="en" :key="key" :toolbars="toolbars" >
+                <template slot="left-toolbar-after">
+                    <button
+                        type="button"
+                        @click="on_btn_save_click"
+                        class="op-icon fa fa-mavon-floppy-o"
+                        aria-hidden="true"
+                        :title="$t('Save')"
+                    ></button>
+                    <button
+                        type="button"
+                        @click="on_btn_add_text('HOLA')"
+                        class="mdi-eye"
+                        aria-hidden="true"
+                        :title="$t('Add text')"
+                    ></button>
+                </template>
+            </mavon-editor>
+        </v-card>
     </div>
 </template>
 <script>
@@ -71,7 +104,7 @@
                     /* 2.2.1 */
                     subfield: true,
                     preview: false
-                }
+                },
             }
         },
         methods: {
@@ -106,15 +139,19 @@
                 };
                 this.$refs.me.insertText(this.$refs.me.getTextareaDom(), insert_text)
                 this.$refs.me.getTextareaDom().selectionStart=this.$refs.me.getTextareaDom().selectionEnd
-            }
+            },
+            on_ingredient_click(item){
+                this.on_btn_add_text(item.fullname)
+            },
+            on_container_click(item){
+                this.on_btn_add_text(item.name)
+            },
         },
         created(){
             if (this.elaboration.elaborations_texts){
                 this.text=this.elaboration.elaborations_texts.text
             }
-            console.log(this.elaboration)
-            console.log(this.text)
+            console.log(this.elaboration.elaborations_containers)
         }
     }
 </script>
-
