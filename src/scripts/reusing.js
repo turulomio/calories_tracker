42f40dd73300a4c37b18406283e4563d06bdd688
turulomio <turulomio@yahoo.es>
2023-08-14 08:08:34 +0200

    Added reusing script

---
+++ src/scripts/reusing.js
@@ -0,0 +1,72 @@
+// To see from npm run : npm run reusing -- --help
+
+const { ArgumentParser } = require('argparse')
+
+
+const parser = new ArgumentParser({ description: 'Update reused files from other projects' })
+parser.parse_args()
+
+
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+async function download(url, filePath) {
+    const proto = !url.charAt(4).localeCompare('s') ? https : http;
+  
+    return new Promise((resolve, reject) => {
+      const file = fs.createWriteStream(filePath);
+      let fileInfo = null;
+  
+      const request = proto.get(url, response => {
+        if (response.statusCode !== 200) {
+          fs.unlink(filePath, () => {
+            reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+          });
+          return;
+        }
+  
+        fileInfo = {
+          mime: response.headers['content-type'],
+          size: parseInt(response.headers['content-length'], 10),
+        };
+  
+        response.pipe(file);
+      });
+  
+      // The destination stream is ended by the time it's called
+      file.on('finish', () => {
+        console.log("Done", url)
+        resolve(fileInfo)
+      });
+  
+      request.on('error', err => {
+        fs.unlink(filePath, () => reject(err));
+      });
+  
+      file.on('error', err => {
+        fs.unlink(filePath, () => reject(err));
+      });
+  
+      request.end();
+    });
+  }
+  
+
+/*var replace_in_file = async function(someFile, search, replace){
+    const data = await fs.promises.readFile(someFile, 'utf8');
+    const result = data.replace(search,replace)
+    await fs.promises.writeFile(someFile, result,'utf8');
+}*/
+
+download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/BtnLogIn.vue", "src/components/reusing/BtnLogIn.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/BtnLogOut.vue", "src/components/reusing/BtnLogOut.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/ChartPie.vue", "src/components/reusing/ChartPie.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/DisplayValues.vue", "src/components/reusing/DisplayValues.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/MyDatePicker.vue", "src/components/reusing/MyDatePicker.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/MyDateTimePicker.vue", "src/components/reusing/MyDateTimePicker.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/MyMenuInline.vue", "src/components/reusing/MyMenuInline.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/BtnSwitchLanguages.vue", "src/components/reusing/BtnSwitchLanguages.vue")
+download("https://raw.githubusercontent.com/turulomio/vipcontacts/main/src/components/PasteImage.vue", "src/components/reusing/PasteImage.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/rules.js", "src/components/reusing/rules.js")
+
