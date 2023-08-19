57a7cabca4040f6a55d8a31c22b503b5a4a3e2e5
turulomio <turulomio@yahoo.es>
2023-08-19 09:43:35 +0200

    Removved suggestion

--- src/components/ElaborationTextTipTap.vue
+++ src/components/ElaborationTextTipTap.vue
@@ -97,12 +97,14 @@
 </template>
 <script>
     import axios from 'axios'
-    import { Editor, EditorContent } from '@tiptap/vue-3'
+    import { Editor, EditorContent,VueRenderer } from '@tiptap/vue-3'
     import StarterKit from '@tiptap/starter-kit'
     import Color from "@tiptap/extension-color";
     import TextStyle from "@tiptap/extension-text-style";
     import Mention from '@tiptap/extension-mention'
-    import suggestion from './suggestion.js'
+    import tippy from 'tippy.js'
+
+    import MentionList from './MentionList.vue'
     export default {
         components: {
             EditorContent,
@@ -161,6 +163,75 @@
             async print () {
               await this.$htmlToPaper("editor");
             },
+            suggestion(){
+              return {
+                items: ({ query }) => {
+                  return [
+                    'Lea Thompson', 'Cyndi Lauper', 'Tom Cruise', 'Madonna', 'Jerry Hall', 'Joan Collins', 'Winona Ryder', 'Christina Applegate', 'Alyssa Milano', 'Molly Ringwald', 'Ally Sheedy', 'Debbie Harry', 'Olivia Newton-John', 'Elton John', 'Michael J. Fox', 'Axl Rose', 'Emilio Estevez', 'Ralph Macchio', 'Rob Lowe', 'Jennifer Grey', 'Mickey Rourke', 'John Cusack', 'Matthew Broderick', 'Justine Bateman', 'Lisa Bonet',
+                  ].filter(item => item.toLowerCase().startsWith(query.toLowerCase())).slice(0, 5)
+                },
+
+                render: () => {
+                  let component
+                  let popup
+
+                  return {
+                    onStart: props => {
+                      console.log("onStart")
+                      component = new VueRenderer(MentionList, {
+                        props,
+                        editor: props.editor,
+                      })
+
+                      if (!props.clientRect) {
+                        return
+                      }
+
+                      popup = tippy('body', {
+                        getReferenceClientRect: props.clientRect,
+                        appendTo: () => document.body,
+                        content: component.element,
+                        showOnCreate: true,
+                        hideOnClick: true,
+                        interactive: true,
+                        trigger: 'manual',
+                        placement: 'bottom-start',
+                      })
+                    },
+
+                    onUpdate(props) {
+                      console.log("onUpdate")
+                      component.updateProps(props)
+
+                      if (!props.clientRect) {
+                        return
+                      }
+
+                      popup[0].setProps({
+                        getReferenceClientRect: props.clientRect,
+                      })
+                    },
+
+                    onKeyDown(props) {
+                      console.log("onkeydown")
+                      if (props.event.key === 'Escape') {
+                        popup[0].hide()
+
+                        return true
+                      }
+
+                      return component.ref?.onKeyDown(props)
+                    },
+
+                    onExit() {
+                      console.log("onExit2")
+                      popup[0].destroy()
+                      component.destroy()
+                    },
+                  }
+                },
+              }
+            }
         },
         created(){
             if (this.elaboration.elaborations_texts){
@@ -177,7 +248,7 @@
                       HTMLAttributes: {
                         class: 'mention',
                       },
-                      suggestion,
+                      suggestion:this.suggestion(),
                     }),
                 ],
                 onUpdate: ({ editor }) => {
