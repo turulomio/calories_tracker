84a475b97cbf7ec4f979f78a0e19784c076a67fd
turulomio <turulomio@yahoo.es>
2023-09-10 22:10:58 +0200

    Improving tiptap ui

--- src/components/ElaborationTextTipTap.vue
+++ src/components/ElaborationTextTipTap.vue
@@ -1,90 +1,37 @@
 <template>
-    <div>
-    <div class="d-flex" >
-
-    <v-card width="100%" height="500">
-
-  <div v-if="editor">
-    <v-btn @click="editor.chain().focus().toggleBold().run()" :disabled="!editor.can().chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
-      bold
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleItalic().run()" :disabled="!editor.can().chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }">
-      italic
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleStrike().run()" :disabled="!editor.can().chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
-      strike
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleCode().run()" :disabled="!editor.can().chain().focus().toggleCode().run()" :class="{ 'is-active': editor.isActive('code') }">
-      code
-    </v-btn>
-    <v-btn @click="editor.chain().focus().unsetAllMarks().run()">
-      clear marks
-    </v-btn>
-    <v-btn @click="editor.chain().focus().clearNodes().run()">
-      clear nodes
-    </v-btn>
-    <v-btn @click="editor.chain().focus().setParagraph().run()" :class="{ 'is-active': editor.isActive('paragraph') }">
-      paragraph
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleHeading({ level: 1 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
-      h1
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleHeading({ level: 2 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
-      h2
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
-      h3
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleHeading({ level: 4 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }">
-      h4
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleHeading({ level: 5 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }">
-      h5
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleHeading({ level: 6 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }">
-      h6
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleBulletList().run()" :class="{ 'is-active': editor.isActive('bulletList') }">
-      bullet list
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleOrderedList().run()" :class="{ 'is-active': editor.isActive('orderedList') }">
-      ordered list
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleCodeBlock().run()" :class="{ 'is-active': editor.isActive('codeBlock') }">
-      code block
-    </v-btn>
-    <v-btn @click="editor.chain().focus().toggleBlockquote().run()" :class="{ 'is-active': editor.isActive('blockquote') }">
-      blockquote
-    </v-btn>
-    <v-btn @click="editor.chain().focus().setHorizontalRule().run()">
-      horizontal rule
-    </v-btn>
-    <v-btn @click="editor.chain().focus().setHardBreak().run()">
-      hard break
-    </v-btn>
-    <v-btn @click="editor.chain().focus().undo().run()" :disabled="!editor.can().chain().focus().undo().run()">
-      undo
-    </v-btn>
-    <v-btn @click="editor.chain().focus().redo().run()" :disabled="!editor.can().chain().focus().redo().run()">
-      redo
-    </v-btn>
-    <v-btn @click="on_btn_save_click" >
-      save
-    </v-btn>
-    <v-btn @click="on_btn_html_code" >
-      {{$t("HTML code")}}
-    </v-btn>
-    <v-btn @click="on_update_mentions" >
-      {{$t("Update mentions")}}
-    </v-btn>
-  </div>
-            <editor-content id="editor" ref="me" :editor="editor" ></editor-content>
-        </v-card>
-    </div>        
+  <div>
+    <div class="d-flex flex-column" >
+      <div v-if="editor">    
+        <v-tooltip :text="$t('Save')" location="bottom"><template v-slot:activator="{ props }"><v-btn v-bind="props" variant="text" icon="mdi-content-save" @click="on_btn_save_click"></v-btn></template></v-tooltip>
+        <v-btn append-icon="mdi-undo"  @click="editor.chain().focus().undo().run()" :disabled="!editor.can().chain().focus().undo().run()"></v-btn>
+        <v-btn append-icon="mdi-redo" @click="editor.chain().focus().redo().run()" :disabled="!editor.can().chain().focus().redo().run()"></v-btn>
+        <v-tooltip :text="$t('Bold text')" location="bottom"><template v-slot:activator="{ props }"><v-btn v-bind="props" variant="text" icon="mdi-format-bold" @click="editor.chain().focus().toggleBold().run()"></v-btn></template></v-tooltip>
+        <v-btn append-icon="mdi-format-italic" @click="editor.chain().focus().toggleItalic().run()" :disabled="!editor.can().chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }"></v-btn>
+        <v-btn append-icon="mdi-format-strikethrough"  @click="editor.chain().focus().toggleStrike().run()" :disabled="!editor.can().chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }" ></v-btn>
+        <v-btn append-icon="mdi-code-greater-than" @click="editor.chain().focus().toggleCode().run()" :disabled="!editor.can().chain().focus().toggleCode().run()" :class="{ 'is-active': editor.isActive('code') }"></v-btn>
+        <v-tooltip :text="$t('Clear marks')" location="bottom"><template v-slot:activator="{ props }"><v-btn v-bind="props" variant="text" icon="mdi-format-clear" @click="editor.chain().focus().unsetAllMarks().run()"></v-btn></template></v-tooltip>
+        <v-btn @click="editor.chain().focus().clearNodes().run()">clear nodes</v-btn>
+        <v-btn append-icon="mdi-format-paragraph" @click="editor.chain().focus().setParagraph().run()" :class="{ 'is-active': editor.isActive('paragraph') }"></v-btn>
+        <v-btn append-icon="mdi-format-header-1"  @click="editor.chain().focus().toggleHeading({ level: 1 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"></v-btn>
+        <v-btn append-icon="mdi-format-header-2"  @click="editor.chain().focus().toggleHeading({ level: 2 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"></v-btn>
+        <v-btn append-icon="mdi-format-header-3"  @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"></v-btn>
+        <v-btn append-icon="mdi-format-list-bulleted" @click="editor.chain().focus().toggleBulletList().run()" :class="{ 'is-active': editor.isActive('bulletList') }"></v-btn>
+        <v-btn append-icon="mdi-format-list-numbered" @click="editor.chain().focus().toggleOrderedList().run()" :class="{ 'is-active': editor.isActive('orderedList') }"></v-btn>
+        <v-btn @click="editor.chain().focus().toggleCodeBlock().run()" :class="{ 'is-active': editor.isActive('codeBlock') }">code block  </v-btn>
+        <v-btn @click="editor.chain().focus().toggleBlockquote().run()" :class="{ 'is-active': editor.isActive('blockquote') }">blockquote</v-btn>
+        <v-btn append-icon="mdi-minus"  @click="editor.chain().focus().setHorizontalRule().run()"></v-btn>
+        <v-btn append-icon="mdi-keyboard-return"  @click="editor.chain().focus().setHardBreak().run()"></v-btn>
+        <v-btn append-icon="mdi-language-html5"  @click="on_btn_html_code" ></v-btn>
+        <v-btn append-icon="mdi-reload"  @click="on_update_mentions" >{{$t("Update mentions")}}</v-btn>
+      </div>
+      <v-card  height="500" style="overflow: auto" class="d-flex flex-row">
+        <editor-content id="editor" ref="me" :editor="editor" ></editor-content>
+      </v-card>
+    </div>    
+        <p v-if="show_html_code"> HTMLCODE {{ html_code }}</p>    
     <p v-if="unused_ingredients()" class="my-2 boldred d-flex justify-center">{{ $t("Products unused : [0]").format(unused_ingredients())}}</p>
 
-        <p v-if="show_html_code"> HTMLCODE {{ html_code }}</p>
-    </div>
+  </div>  
 </template>
 <script>
     import axios from 'axios'
@@ -398,7 +345,7 @@
 <style>
   .mention_containers{
     color: rgb(15, 15, 139);
-    background-color: rgb(135, 185, 231);
+    background-color: rgb(165, 197, 228);
   }
 
   ol > li {
@@ -414,8 +361,8 @@
   }
 
   .mention_ingredients {
-    color: #320b70;
-    background-color: #857d92;
+    color: #3f310b;
+    background-color: #f7dbbc;
     border-radius: 0.3rem;
     padding: 0.1rem 0.3rem;
   }
