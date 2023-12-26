435895c218f0817b140f6b4817302adf07765f1c
turulomio <turulomio@yahoo.es>
2023-12-27 00:29:44 +0100

    npm run reusing

--- src/scripts/reusing.js
+++ src/scripts/reusing.js
@@ -1,15 +1,19 @@
 // To see from npm run : npm run reusing -- --help
 
-const { ArgumentParser } = require('argparse')
+import {ArgumentParser} from "argparse"
+// const { ArgumentParser } = require('argparse')
 
 
 const parser = new ArgumentParser({ description: 'Update reused files from other projects' })
 parser.parse_args()
 
 
-var http = require('http');
-var https = require('https');
-var fs = require('fs');
+import http from "http"
+import https from "https"
+import fs from "fs"
+// var http = require('http');
+// var https = require('https');
+// var fs = require('fs');
 
 async function download(url, filePath) {
     const proto = !url.charAt(4).localeCompare('s') ? https : http;
@@ -58,13 +62,12 @@ async function download(url, filePath) {
     await fs.promises.writeFile(someFile, result,'utf8');
 }*/
 
-download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/BtnLogIn.vue", "src/components/reusing/BtnLogIn.vue")
-download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/BtnLogOut.vue", "src/components/reusing/BtnLogOut.vue")
-download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/ChartPie.vue", "src/components/reusing/ChartPie.vue")
-download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/DisplayValues.vue", "src/components/reusing/DisplayValues.vue")
-download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/MyDatePicker.vue", "src/components/reusing/MyDatePicker.vue")
-download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/MyDateTimePicker.vue", "src/components/reusing/MyDateTimePicker.vue")
-download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/MyMenuInline.vue", "src/components/reusing/MyMenuInline.vue")
-download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/components/BtnSwitchLanguages.vue", "src/components/reusing/BtnSwitchLanguages.vue")
-download("https://raw.githubusercontent.com/turulomio/moneymoney3o/main/src/rules.js", "src/components/reusing/rules.js")
+download("https://raw.githubusercontent.com/turulomio/moneymoney/main/src/components/BtnLogIn.vue", "src/components/reusing/BtnLogIn.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney/main/src/components/BtnLogOut.vue", "src/components/reusing/BtnLogOut.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney/main/src/components/ChartPie.vue", "src/components/reusing/ChartPie.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney/main/src/components/DisplayValues.vue", "src/components/reusing/DisplayValues.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney/main/src/components/MyDatePicker.vue", "src/components/reusing/MyDatePicker.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney/main/src/components/MyDateTimePicker.vue", "src/components/reusing/MyDateTimePicker.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney/main/src/components/MyMenuInline.vue", "src/components/reusing/MyMenuInline.vue")
+download("https://raw.githubusercontent.com/turulomio/moneymoney/main/src/components/BtnSwitchLanguages.vue", "src/components/reusing/BtnSwitchLanguages.vue")
 
