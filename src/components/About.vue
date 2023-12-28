6ef3276dfc0799cf6a84dc05510c5303c5e5e039
turulomio <turulomio@yahoo.es>
2023-12-28 18:08:20 +0100

    Moved styles to app

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,22 +2,10 @@
     <div class="paragraph">
         <h1>{{ $t(`About Calories Tracker`) }}</h1>
         <h2>{{ `${useStore().version} (${useStore().versiondate.toISOString().slice(0,10)})` }}</h2>
-        
-            <p>{{ $t("Calories Tracker is a personal finances application") }}</p>
+        <p class="paragraph_centered">{{ $t("Manage your meals, your recipes, your calories and your biometric data.") }}</p>
     </div>
 </template>
 
 <script setup>
     import { useStore } from '../store.js'
 </script>
-<style>
-h2 {
-    text-align: center;
-    font-weight: normal;
-}
-
-paragraph{
-    text-align:justify;
-    padding: 30px;
-}
-</style>
