bca44ea5d7b73686a832cdd1e2303ee0ad5ff1f9
turulomio <turulomio@yahoo.es>
2024-08-07 10:26:58 +0200

    New shopping list system without unogenerator

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -7,7 +7,7 @@
             </v-form>
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary" @click="acceptDialog()">{{ $t("Get shopping list") }}</v-btn>
+                <v-btn color="primary" @click="acceptDialog" :loading="creating" :disabled="creating || elaborations_selected.length==0">{{ $t("Get shopping list") }}</v-btn>
             </v-card-actions>
         </v-card>
     </div>
@@ -15,29 +15,69 @@
 <script>
     import axios from 'axios'
     import { useStore } from '@/store.js'
+    import pdfMake from "pdfmake/build/pdfmake";
+    import pdfFonts from "pdfmake/build/vfs_fonts";
+    pdfMake.addVirtualFileSystem(pdfFonts);
+
     export default {
         data(){ 
             return{
                 form_valid:false,
                 elaborations:[],
-                elaborations_selected:[]
+                elaborations_selected:[],
+                creating:false, 
             }
         },
         methods: {
-        useStore,
-            acceptDialog(){       
+            useStore,
+            async acceptDialog(){       
                 if (this.form_valid!=true) {
                     this.$refs.form.validate()
                     return
                 }
                 axios.post(`${this.useStore().apiroot}/shopping_list/`, {elaborations: this.elaborations_selected}, this.myheaders())
                 .then((response) => {
-                    var link = window.document.createElement('a')
-                    link.href = `data:${response.data.data.mime};base64,${response.data.data.data}`
-                    link.download = response.data.data.filename
-                    document.body.appendChild(link)
-                    link.click()
-                    document.body.removeChild(link)
+
+                    console.log(response.data)
+
+                    var shopping_list=[]
+                    response.data.shopping_list.forEach(o => {
+                        shopping_list.push(`${o.product_fullname} (${o.grams})`)
+                        
+                    });
+
+
+                    this.creating=true
+                    const docDefinition = {
+                        info: {
+                            title: this.$t("Shopping list"),
+                            author: `Calories Tracker v${this.useStore().version}`,
+                            subject: this.$t("Calories Tracker shopping list"),
+                            keywords: 'shopping list',
+                        },
+                        content: [    
+                            { text: 'Shopping list', style: 'header1', alignment:'center' },
+                            { text: 'Recipes', style: 'header2', alignment:'center' },
+                            { ul: response.data.recipes},
+                            { text: 'Shopping list', style: 'header2', alignment:'center' },
+                            { ul: shopping_list},
+                        ],
+                        styles: {
+                            header1: { fontSize: 16, bold: true , margin: [0, 6, 0, 6]},
+                            header2: { fontSize: 14, bold: true , margin: [6, 4, 0, 4]},
+                            body: { fontSize: 11 ,margin:[0,2,0,2], alignment:"justify"},
+                        },
+
+                        footer: function(currentPage, pageCount) {
+                            return (currentPage>2)? { text: currentPage.toString() + this.$t(' of ') + pageCount, alignment: 'center' }:""
+                        }.bind(this)
+                    };
+                    console.log("PDFMAKE", docDefinition)
+
+                    var filename="Shopping List.pdf"
+                    pdfMake.createPdf(docDefinition,{tagged:true}).download(filename);
+                    this.creating=false
+
                 }, (error) => {
                     this.parseResponseError(error)
                 })
--- src/i18n.js
+++ src/i18n.js
@@ -2,34 +2,9 @@
 import { createI18n } from 'vue-i18n'
 import json_es from './locales/es.json'
 
-// Vue.use(VueI18n)
-
-// function loadLocaleMessages () {
-//   const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-//   const messages = {}
-//   locales.keys().forEach(key => {
-//     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-//     if (matched && matched.length > 1) {
-//       const locale = matched[1]
-//       messages[locale] = locales(key)
-//     }
-//   })
-//   return messages
-// }
-
-// export default new VueI18n({
-//   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-//   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-//   silentTranslationWarn: true,
-//   messages: loadLocaleMessages()
-// })
-
-
-
-// 2. Create i18n instance with options
 const i18n=createI18n({
   globalInjection: true,
-  legacy:true,
+  legacy: true, // Set to false to use Composition API
   locale: 'en', // set locale
   fallbackLocale: 'en', // set fallback locale
   silentFallbackWarn: true,
