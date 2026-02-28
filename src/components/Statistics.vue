42ae92136379c16c22619b72a8e4b82df0f66267
turulomio <turulomio@yahoo.es>
2026-02-28 09:40:54 +0100

    Improved statistics chart

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,39 +1,86 @@
 <template>
     <div v-show="this.useStore().logged">
             <h1>{{ $t("Statistics") }}</h1>
-            <chart-pie :name="$t('Calories Tracker registers')" :items="items" :height="600" :key="key"></chart-pie>
+            <p class="ma-4 text-center">{{ $t("Here you can see the distribution of the registers in the application") }}</p>
+            <div ref="chart_div" style="width: 100%; height: 600px;"></div>
     </div>
 </template>
 
 <script>
-    import ChartPie from './reusing/ChartPie.vue'
+    import * as echarts from 'echarts';
     import axios from "axios"
     import { useStore } from '@/store.js'
     export default {
-        components: {
-            ChartPie,
-        },
         data () {
             return {
-                items:[],
-                key:0,
+                resizeObserver: null,
             }
         },
         methods: {
-        useStore,
+            useStore,
             getStatistics(){
                 axios.get(`${this.useStore().apiroot}/statistics/`, this.myheaders())
                 .then((response) => {
                     this.parseResponse(response)
-                    this.items=response.data 
+                    this.initChart(response.data)
                 }, (error) => {
                     this.parseResponseError(error)
                 })
-                .finally(() => (this.isLoading = false));
+            },
+            initChart(data) {
+                if (!this.$refs.chart_div) return;
+
+                const option = {
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {
+                            type: 'shadow'
+                        }
+                    },
+                    grid: {
+                        containLabel: true
+                    },
+                    xAxis: {
+                        type: 'log'
+                    },
+                    yAxis: {
+                        type: 'category',
+                        data: data.map(x => x.name),
+                        inverse: true
+                    },
+                    series: [
+                        {
+                            name: this.$t('Calories Tracker registers'),
+                            type: 'bar',
+                            data: data.map(x => x.value),
+                            colorBy: 'data',
+                            label: {
+                                show: true,
+                                position: 'right'
+                            }
+                        }
+                    ]
+                };
+
+                if (!this.chart) {
+                    this.chart = echarts.init(this.$refs.chart_div);
+                }
+                this.chart.setOption(option);
             }
         },
         mounted(){
-            this.getStatistics()
+            this.getStatistics();
+            this.resizeObserver = new ResizeObserver(() => {
+                if (this.chart) this.chart.resize();
+            });
+            this.resizeObserver.observe(this.$refs.chart_div);
+        },
+        beforeUnmount() {
+            if (this.resizeObserver) this.resizeObserver.disconnect();
+            if (this.chart) {
+                this.chart.dispose();
+                this.chart = null;
+            }
         }
     }
 </script>
