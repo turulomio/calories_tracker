ac3d7be5e7ec82c960a0fb675076a85fd9ab0876
turulomio <turulomio@yahoo.es>
2026-05-10 11:06:01 +0200

    npm update. Recipes by ingrediente now works with :INGREDIENTSALL and :INGREDIENTSANY (#261)
    * npm update
    
    * Trying validation
    
    * Added form validation. Now works with :INGREDIENTSALL :INGREDIENTSANY

--- src/components/AutocompleteProducts.vue
+++ src/components/AutocompleteProducts.vue
@@ -1,5 +1,16 @@
 <template>
-    <v-autocomplete :readonly="readonly" :items="new_products" v-model="new_value" :label="mylabel"  item-title="fullname" :return-object="returnObject" :item-value="(multiple)? 'id': 'url'" :multiple="multiple">
+    <v-autocomplete
+      v-bind="$attrs"
+      :rules="rules"
+      :readonly="readonly"
+      :items="computedProducts"
+      v-model="proxyValue"
+      :label="mylabel"
+      item-title="fullname"
+      :return-object="returnObject"
+      :item-value="(multiple)? 'id': 'url'"
+      :multiple="multiple"
+    >
       <template v-slot:item="{ props, item }">
             <v-list-item v-bind="props" title=""><div v-html="products_html_fullname(item,2)"></div></v-list-item>
         </template>         
@@ -9,68 +20,38 @@
     </v-autocomplete>
 </template>
 
-<script>
-
+<script setup>
+import { computed, useAttrs } from 'vue'
 import { useStore } from '@/store.js'
-export default {
-    name: "AutocompleteProducts",
-    props:{
-        modelValue: { 
-            required: true,
-        },
-        returnObject:{
-            type: Boolean,
-            required:false,
-            default:false,
-        },
-        readonly: {
-            type: Boolean,
-            required: false,
-            default: false
-        },
-        products: {
-            type: Array,
-            required:false,
-            default: new Array(),
-        },
+import { getArrayFromMap, products_html_fullname } from '@/functions.js'
 
-        multiple: { //Used to select several products, returns a list of ids
-            type: Boolean,
-            required: false,
-            default: false
-        },
-    },
-    emits: ['update:modelValue'],
-    computed:{
-        mylabel(){
-            if ('label' in this.$attrs) return this.$attrs.label
-            return this.$t('Select a product')
-        }
-    },
-    data(){ 
-        return {
-            new_value:null,
-            new_products: null,
-        }
-    },
-    watch:{
-        value(newValue){
-            this.new_value=newValue
-        },
-        new_value(newValue){
-            this.$emit('update:modelValue', newValue)
-        },
-    },
-    methods:{
-        useStore,
-    },
-    created(){
-        this.new_value=this.modelValue
-        if (this.products.length==0){//Default value
-            this.new_products=this.getArrayFromMap(this.useStore().products)
-        } else {
-            this.new_products=this.products
-        }
+const props = defineProps({
+    modelValue: { required: true },
+    returnObject: { type: Boolean, default: false },
+    readonly: { type: Boolean, default: false },
+    products: { type: Array, default: () => [] },
+    multiple: { type: Boolean, default: false },
+    rules: { type: Array, default: () => [] },
+})
+
+const emit = defineEmits(['update:modelValue'])
+const attrs = useAttrs()
+const store = useStore()
+
+const proxyValue = computed({
+    get: () => props.modelValue,
+    set: (val) => emit('update:modelValue', val)
+})
+
+const computedProducts = computed(() => {
+    if (!props.products || props.products.length === 0) {
+        return getArrayFromMap(store.products)
     }
-}
+    return props.products
+})
+
+const mylabel = computed(() => {
+    if (attrs.label) return attrs.label
+    return 'Select a product'
+})
 </script>
